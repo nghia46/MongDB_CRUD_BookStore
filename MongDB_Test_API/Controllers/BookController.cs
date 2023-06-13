d8b73fcc6973c871272e3a6afa4b99d250a04349
tiptopdragon0406 <84240893+tiptopdragon0406@users.noreply.github.com>
2023-06-13 19:37:56 +0700

    Add project files.

---
+++ MongDB_Test_API/Controllers/BookController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Repository.Model;
+
+namespace MongDB_Test_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private IMongoCollection<Book> _bookCollection;
+        public BookController(IMongoClient client)
+        {
+            var database = client.GetDatabase("BookStoreDB");
+            _bookCollection = database.GetCollection<Book>("Books");
+        }
+        [HttpGet("GetAllBook")]
+        public IActionResult getAll()
+        {
+            List<Book> books = _bookCollection.Find(_ => true).ToList(); // Retrieve all documents from the collection
+            return Ok(books);
+        }
+        [HttpGet("GetBookByID/{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            var filter = Builders<Book>.Filter.Eq("_id", id);
+            Object book = _bookCollection.Find(filter).FirstOrDefault();
+
+            if (book == null)
+            {
+                return NotFound("Book not found.");
+            }
+
+            return Ok(book);
+        }
+        [HttpPost("AddNewBook")]
+        public IActionResult Add([FromBody] Book book)
+        {
+            book.Book_ID = Guid.NewGuid(); ;
+            _bookCollection.InsertOne(book);
+            return Ok("Book added successfully.");
+        }
+
+        [HttpDelete("DelelteByID/{id}")]
+        public IActionResult DeleteById(Guid id)
+        {
+            var filter = Builders<Book>.Filter.Eq("_id", id);
+            var result = _bookCollection.DeleteOne(filter);
+
+            if (result.DeletedCount == 0)
+            {
+                return NotFound("Book not found.");
+            }
+
+            return Ok("Book deleted successfully.");
+        }
+
+        [HttpPut("UpdateByID/{id}")]
+        public IActionResult UpdateById2(Guid id, [FromBody] Book updatedBook)
+        {
+            var filter = Builders<Book>.Filter.Eq("_id", id);
+
+            var update = Builders<Book>.Update
+                .Set("Name", updatedBook.Name)
+                .Set("Author", updatedBook.Author)
+                .Set("Price", updatedBook.Price);
+
+            var options = new UpdateOptions { IsUpsert = false };
+
+            var result = _bookCollection.UpdateOne(filter, update, options);
+
+            if (result.ModifiedCount == 0)
+            {
+                return NotFound("Book not found.");
+            }
+
+            return Ok("Book updated successfully.");
+        }
+
+    }
+
+}
---
+++ Repository/Model/Book.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Model
+{
+    [BsonIgnoreExtraElements]
+    public class Book
+    {
+        [BsonId]
+        public Guid Book_ID { get; set; }
+        [BsonElement("Name")]
+        public string Name { get; set; }
+        [BsonElement("Author")]
+        public string Author { get; set; }
+        [BsonElement("Price")]
+        public double Price { get; set; }
+    }
+}
